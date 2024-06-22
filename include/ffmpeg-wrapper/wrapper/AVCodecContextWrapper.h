fa9a209de8926c1bf9b888e4df2b57ce6dd80308
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 00:34:30 +0800

    1

--- include/ffmpeg-wrapper/wrapper/AVCodecContextWrapper.h
+++ include/ffmpeg-wrapper/wrapper/AVCodecContextWrapper.h
@@ -62,31 +62,32 @@ namespace video
 		}
 
 #pragma region 工厂函数
-		/// <summary>
-		///		创建解码器。此函数创建的解码器是通用的，音频流和视频流的解码器都是用这个函数创建。
-		///		* 会将流的 AVCodecParameters 复制到解码器上下文中
-		///		* 会将流的时间基复制到解码器上下文中。
-		///		* 会将流的帧率复制到解码器上下文中。（虽然帧率只对视频流有效，但音频流反正不会用到，复制了也没事）
-		///		* 返回的解码器将处于打开状态
-		/// </summary>
-		/// <param name="stream"></param>
-		/// <returns></returns>
+		/**
+		 * @brief 创建解码器。此函数创建的解码器是通用的，音频流和视频流的解码器都是用这个函数创建。
+		 *
+		 * @note 会将流的 AVCodecParameters 复制到解码器上下文中
+		 * @note 会将流的时间基复制到解码器上下文中。
+		 * @note 会将流的帧率复制到解码器上下文中。（虽然帧率只对视频流有效，但音频流反正不会用到，复制了也没事）
+		 * @note 返回的解码器将处于打开状态
+		 *
+		 * @param stream
+		 * @return std::shared_ptr<AVCodecContextWrapper>
+		 */
 		static std::shared_ptr<AVCodecContextWrapper> CreateDecoder(AVStreamInfoCollection stream);
 
-		/// <summary>
-		///		创建音频编码器
-		/// </summary>
-		/// <param name="encoder_name">通过编码器的名称查找编码器</param>
-		/// <param name="infos">
-		///		音频流信息。将会利用此流的信息创建编码器。
-		///		* 所利用的信息包括时间基。
-		/// </param>
-		/// <param name="set_global_header">
-		///		是否需要设置全局头部。
-		///		* 某些封装格式要求编码器设置全局头部。可以通过检查 AVFormatContextWrapper 的
-		///		  NeedGlobalHeader 属性来确定是否需要全局头部。
-		/// </param>
-		/// <returns></returns>
+		/**
+		 * @brief 创建音频编码器
+		 *
+		 * @param encoder_name 通过编码器的名称查找编码器
+		 * @param infos 音频流信息。将会利用此流的信息创建编码器。* 所利用的信息包括时间基。
+		 *
+		 * @param set_global_header 是否需要设置全局头部。
+		 * - 某些封装格式要求编码器设置全局头部。可以通过检查 AVFormatContextWrapper 的
+		 * 	 NeedGlobalHeader 属性来确定是否需要全局头部。
+		 *
+		 * @param auto_open
+		 * @return std::shared_ptr<AVCodecContextWrapper>
+		 */
 		static std::shared_ptr<AVCodecContextWrapper> CreateEncoder(
 			const char *encoder_name,
 			IAudioStreamInfoCollection const &infos,
@@ -95,12 +96,13 @@ namespace video
 
 		/**
 		 * @brief 创建视频编码器。返回的编码器已打开。
+		 *
 		 * @param encoder_name 通过编码器的名称查找编码器
 		 * @param infos 视频流信息。将会利用此流的信息创建编码器。所利用的信息包括时间基。
 		 *
 		 * @param set_global_header 是否需要设置全局头部。
-		 * * 某些封装格式要求编码器设置全局头部。可以通过检查 AVFormatContextWrapper 的
-		 * NeedGlobalHeader 属性来确定是否需要全局头部。
+		 * - 某些封装格式要求编码器设置全局头部。可以通过检查 AVFormatContextWrapper 的
+		 *   NeedGlobalHeader 属性来确定是否需要全局头部。
 		 *
 		 * @return 返回的编码器已打开。
 		 */
@@ -110,12 +112,11 @@ namespace video
 			bool set_global_header,
 			bool auto_open = true);
 #pragma endregion
-
-		/// <summary>
-		///		打开编解码器。
-		///		- 打开前一定要先设置编解码器的参数。
-		/// </summary>
-		/// <param name="dic"></param>
+		/**
+		 * @brief 打开编解码器。
+		 * @note 打开前一定要先设置编解码器的参数。
+		 * @param dic
+		 */
 		void Open(AVDictionary **dic = nullptr);
 
 		void SetCodecParams(AVCodecParameters *param);
@@ -123,8 +124,8 @@ namespace video
 		/**
 		 * @brief 设置全局头部
 		 *
-		 * - 某些封装格式要求编码器在编码的时候设置全局头部。要知道封装格式是不是需要设置全局头部，
-		 *   可以查看 AVFormatContextWrapper 的 NeedGlobalHeader 属性。
+		 * @note 某些封装格式要求编码器在编码的时候设置全局头部。要知道封装格式是不是需要设置全局头部，
+		 *   	 可以查看 AVFormatContextWrapper 的 NeedGlobalHeader 属性。
 		 */
 		void SetGlobalHeader()
 		{
@@ -161,8 +162,9 @@ namespace video
 
 		/**
 		 * @brief 获取此码器的时间基。
-		 * - 作为编码器，时间基必须由用户手动设置
-		 * - 作为解码器，时间基是无用的，无效的，不要试图从解码器中获取此参数。
+		 *
+		 * @note 作为编码器，时间基必须由用户手动设置
+		 * @note 作为解码器，时间基是无用的，无效的，不要试图从解码器中获取此参数。
 		 *
 		 * @return AVRational
 		 */
