08dac123565c518c454f867b0b3de6a2f4a3fcdf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-22 22:09:29 +0800

    使用clang-format

--- include/ffmpeg-wrapper/mux/SptsEncodeMux.cpp
+++ include/ffmpeg-wrapper/mux/SptsEncodeMux.cpp
@@ -1,13 +1,13 @@
-#include"ffmpeg-wrapper/mux/SptsEncodeMux.h"
-#include<base/container/Queue.h>
-#include<ffmpeg-wrapper/factory/IEncoderPipeFactory.h>
-#include<ffmpeg-wrapper/info-collection/AudioStreamInfoCollection.h>
-#include<ffmpeg-wrapper/info-collection/VideoStreamInfoCollection.h>
-#include<ffmpeg-wrapper/output-format/OutputFormat.h>
-#include<ffmpeg-wrapper/pipe/SwrEncoderPipe.h>
-#include<ffmpeg-wrapper/pipe/SwsFpsEncoderPipe.h>
-#include<iostream>
-#include<jccpp/TaskCompletionSignal.h>
+#include "ffmpeg-wrapper/mux/SptsEncodeMux.h"
+#include <base/container/Queue.h>
+#include <ffmpeg-wrapper/factory/IEncoderPipeFactory.h>
+#include <ffmpeg-wrapper/info-collection/AudioStreamInfoCollection.h>
+#include <ffmpeg-wrapper/info-collection/VideoStreamInfoCollection.h>
+#include <ffmpeg-wrapper/output-format/OutputFormat.h>
+#include <ffmpeg-wrapper/pipe/SwrEncoderPipe.h>
+#include <ffmpeg-wrapper/pipe/SwsFpsEncoderPipe.h>
+#include <iostream>
+#include <jccpp/TaskCompletionSignal.h>
 
 using namespace video;
 using namespace std;
@@ -41,9 +41,7 @@ video::SptsEncodeMux::SptsEncodeMux(
 	int64_t video_out_bitrate_in_bps,
 	// 音频相关参数
 	IAudioStreamInfoCollection const &audio_stream_infos,
-	std::string audio_codec_name
-) :
-	_context(new Context{ })
+	std::string audio_codec_name) : _context(new Context{})
 {
 	_context->_factory = factory;
 	_context->_out_format = out_format;
@@ -58,8 +56,8 @@ video::SptsEncodeMux::SptsEncodeMux(
 	_context->_audio_codec_name = audio_codec_name;
 
 	// ts 必须使用 1/90000 时间基
-	_context->_video_stream_infos.SetTimeBase(AVRational{ 1, 90000 });
-	_context->_audio_stream_infos.SetTimeBase(AVRational{ 1, 90000 });
+	_context->_video_stream_infos.SetTimeBase(AVRational{1, 90000});
+	_context->_audio_stream_infos.SetTimeBase(AVRational{1, 90000});
 
 	InitVideoEncodePipe();
 	InitAudioEncodePipe();
@@ -68,23 +66,23 @@ video::SptsEncodeMux::SptsEncodeMux(
 
 void video::SptsEncodeMux::InitVideoEncodePipe()
 {
-	_context->_video_encoder_pipe = shared_ptr<SwsFpsEncoderPipe>{ new SwsFpsEncoderPipe {
+	_context->_video_encoder_pipe = shared_ptr<SwsFpsEncoderPipe>{new SwsFpsEncoderPipe{
 		_context->_factory,
 		_context->_out_format,
 		_context->_video_stream_infos,
 		_context->_video_codec_name,
 		_context->_video_out_bitrate_in_bps,
-	} };
+	}};
 }
 
 void video::SptsEncodeMux::InitAudioEncodePipe()
 {
-	_context->_audio_encode_pipe = shared_ptr<SwrEncoderPipe>{ new SwrEncoderPipe {
+	_context->_audio_encode_pipe = shared_ptr<SwrEncoderPipe>{new SwrEncoderPipe{
 		_context->_factory,
 		_context->_audio_codec_name,
 		_context->_audio_stream_infos,
 		_context->_out_format,
-	} };
+	}};
 }
 
 void video::SptsEncodeMux::WriteHeader()
@@ -108,13 +106,12 @@ shared_ptr<IFrameConsumer> video::SptsEncodeMux::AudioEncodePipe()
 	return _context->_audio_encode_pipe;
 }
 
-
-#include<ffmpeg-wrapper/input-format/InputFormat.h>
-#include<jccpp/stream/FileStream.h>
-#include<ffmpeg-wrapper/demux/JoinedInputFormatDemuxDecoder.h>
-#include<ffmpeg-wrapper/output-format/StreamOutputFormat.h>
-#include<base/task/CancellationTokenSource.h>
-#include<ffmpeg-wrapper/factory/EncoderPipeFactory.h>
+#include <base/task/CancellationTokenSource.h>
+#include <ffmpeg-wrapper/demux/JoinedInputFormatDemuxDecoder.h>
+#include <ffmpeg-wrapper/factory/EncoderPipeFactory.h>
+#include <ffmpeg-wrapper/input-format/InputFormat.h>
+#include <ffmpeg-wrapper/output-format/StreamOutputFormat.h>
+#include <jccpp/stream/FileStream.h>
 
 /// <summary>
 ///		测试函数
@@ -126,19 +123,18 @@ void test_SptsEncodeMux()
 	file_queue.Enqueue("越权访问.mkv");
 	file_queue.Enqueue("moon.mp4");
 	file_queue.Enqueue("fallen-down.ts");
-	shared_ptr<JoinedInputFormatDemuxDecoder> joined_input_format_demux_decoder{ new JoinedInputFormatDemuxDecoder { } };
-	joined_input_format_demux_decoder->_get_format_callback = [&]()->shared_ptr<InputFormat>
+	shared_ptr<JoinedInputFormatDemuxDecoder> joined_input_format_demux_decoder{new JoinedInputFormatDemuxDecoder{}};
+	joined_input_format_demux_decoder->_get_format_callback = [&]() -> shared_ptr<InputFormat>
 	{
 		try
 		{
 			std::string file = file_queue.Dequeue();
-			shared_ptr<InputFormat> in_fmt_ctx{ new InputFormat { file } };
+			shared_ptr<InputFormat> in_fmt_ctx{new InputFormat{file}};
 			in_fmt_ctx->DumpFormat();
 			return in_fmt_ctx;
 		}
 		catch (std::runtime_error &e)
 		{
-
 		}
 		catch (std::exception &e)
 		{
@@ -154,7 +150,7 @@ void test_SptsEncodeMux()
 
 	// 想要输出的视频信息
 	VideoStreamInfoCollection output_video_stream_infos;
-	output_video_stream_infos._frame_rate = AVRational{ 30, 1 };
+	output_video_stream_infos._frame_rate = AVRational{30, 1};
 	output_video_stream_infos._width = 1920;
 	output_video_stream_infos._height = 1080;
 	output_video_stream_infos._pixel_format = AVPixelFormat::AV_PIX_FMT_YUV420P;
@@ -166,24 +162,23 @@ void test_SptsEncodeMux()
 	output_audio_stream_infos._sample_rate = 48000;
 
 	shared_ptr<base::Stream> out_fs = jccpp::FileStream::CreateNewAnyway("mux_out.ts");
-	shared_ptr<StreamOutputFormat> out_fmt_ctx{ new StreamOutputFormat { ".ts", out_fs } };
-	shared_ptr<SptsEncodeMux> spts_encode_mux{ new SptsEncodeMux {
+	shared_ptr<StreamOutputFormat> out_fmt_ctx{new StreamOutputFormat{".ts", out_fs}};
+	shared_ptr<SptsEncodeMux> spts_encode_mux{new SptsEncodeMux{
 		video::EncoderPipeFactory::Instance(),
 		out_fmt_ctx,
 		output_video_stream_infos,
 		"hevc_amf",
 		-1,
 		output_audio_stream_infos,
-		"eac3"
-	} };
+		"eac3"}};
 
 	joined_input_format_demux_decoder->AddVideoFrameConsumer(spts_encode_mux->VideoEncodePipe());
 	joined_input_format_demux_decoder->AddAudioFrameConsumer(spts_encode_mux->AudioEncodePipe());
 
 	base::CancellationTokenSource cancel_pump_source;
-	TaskCompletionSignal pump_thread_exit{ false };
+	TaskCompletionSignal pump_thread_exit{false};
 	std::thread([&]()
-	{
+				{
 		try
 		{
 			joined_input_format_demux_decoder->Pump(cancel_pump_source.Token());
@@ -198,8 +193,8 @@ void test_SptsEncodeMux()
 		}
 
 		cout << "线程退出" << endl;
-		pump_thread_exit.SetResult();
-	}).detach();
+		pump_thread_exit.SetResult(); })
+		.detach();
 
 	cin.get();
 	cancel_pump_source.Cancel();
