22b358b5902d458f756c711e97f122e4b30df103
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 13:40:28 +0800

    1

--- include/ffmpeg-wrapper/filter/VideoFilterGraph.h
+++ include/ffmpeg-wrapper/filter/VideoFilterGraph.h
@@ -1,17 +1,19 @@
 #pragma once
-#include<ffmpeg-wrapper/info-collection/VideoStreamInfoCollection.h>
-#include<ffmpeg-wrapper/pipe/interface/IFrameConsumer.h>
-#include<ffmpeg-wrapper/pipe/interface/IFrameSource.h>
-#include<ffmpeg-wrapper/wrapper/AVDictionaryWrapper.h>
-#include<ffmpeg-wrapper/wrapper/AVFilterContextWrapper.h>
+#include <ffmpeg-wrapper/info-collection/VideoStreamInfoCollection.h>
+#include <ffmpeg-wrapper/pipe/interface/IFrameConsumer.h>
+#include <ffmpeg-wrapper/pipe/interface/IFrameSource.h>
+#include <ffmpeg-wrapper/wrapper/AVDictionaryWrapper.h>
+#include <ffmpeg-wrapper/wrapper/AVFilterContextWrapper.h>
 
 namespace video
 {
-	class VideoFilterGraph :
-		public base::Wrapper<AVFilterGraph>,
-		public IFrameConsumer,
-		public IFrameSource
+	/// @brief 视频滤镜图
+	class VideoFilterGraph
+		: public base::Wrapper<AVFilterGraph>,
+		  public IFrameConsumer,
+		  public IFrameSource
 	{
+	private:
 		AVFilterGraph *_wrapped_obj = nullptr;
 		VideoStreamInfoCollection _input_video_stream_infos;
 		AVFilterContextWrapper _buffer_filter;
@@ -31,23 +33,17 @@ namespace video
 			return _wrapped_obj;
 		}
 
-		/// <summary>
-		///		获取本滤镜图的起点 —— buffer_filter
-		/// </summary>
-		/// <returns></returns>
+		/// @brief 获取本滤镜图的起点
+		/// @return
 		AVFilterContextWrapper &buffer_filter();
 
-		/// <summary>
-		///		获取本滤镜图的终点 —— buffer_sink_filter
-		/// </summary>
-		/// <returns></returns>
+		/// @brief 获取本滤镜图的终点
+		/// @return
 		AVFilterContextWrapper &buffer_sink_filter();
 
-		/// <summary>
-		///		构造一个 fps 滤镜
-		/// </summary>
-		/// <param name="FrameRate"></param>
-		/// <returns></returns>
+		/// @brief 构造一个 fps 滤镜
+		/// @param FrameRate
+		/// @return
 		AVFilterContextWrapper alloc_fps_filter(AVRational FrameRate);
 
 		void config_graph();
