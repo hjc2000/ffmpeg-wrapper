60e94c16f9f79151a17a97097b7ff4031e34a8b5
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 21:13:48 +0800

    1

--- include/ffmpeg-wrapper/pipe/interface/IFrameSource.h
+++ include/ffmpeg-wrapper/pipe/interface/IFrameSource.h
@@ -8,7 +8,6 @@ namespace video
 	public:
 		virtual ~IFrameSource() = default;
 
-	public:
 		/// @brief 读取帧
 		/// @param frame 用来接收数据的帧
 		/// @return 成功返回 0，失败返回负数的错误代码
--- include/ffmpeg-wrapper/signal-source/ISignalSource.h
+++ include/ffmpeg-wrapper/signal-source/ISignalSource.h
@@ -8,28 +8,22 @@
 
 namespace video
 {
-	/// <summary>
-	///		信号源接口。
-	///		* 一个信号源，要能够采样，所以有一个 sample 纯虚函数。此外，信号源实现了 IFrameSource 接口，
-	///		  能够填充一个 AVFrame
-	/// </summary>
+	/// @brief 信号源接口。
+	/// @note 一个信号源，要能够采样，所以有一个 sample 纯虚函数。此外，信号源实现了 IFrameSource 接口，
+	/// 能够填充一个 AVFrame
 	class ISignalSource
 		: public IAudioFrameInfoCollection
 	{
 	public:
 		virtual ~ISignalSource() = default;
 
-	public:
-		/**
-		 * @brief 采样一次，返回采样结果
-		 */
+		/// @brief 采样一次，返回采样结果
+		/// @return
 		virtual double sample() = 0;
 
-		/// <summary>
-		///		从信号源中读取帧
-		/// </summary>
-		/// <param name="frame">用来接收本信号源采样值的帧</param>
-		/// <returns>目前总是会返回 0</returns>
+		/// @brief 从信号源中读取帧
+		/// @param frame 用来接收本信号源采样值的帧
+		/// @return
 		int ReadFrame(std::shared_ptr<AVFrameWrapper> frame);
 	};
 }
