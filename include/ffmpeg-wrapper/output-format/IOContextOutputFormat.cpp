cdbe382a2097f1c32b08b456caf3d2caea71519c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 13:42:59 +0800

    1

--- include/ffmpeg-wrapper/output-format/IOContextOutputFormat.cpp
+++ include/ffmpeg-wrapper/output-format/IOContextOutputFormat.cpp
@@ -1,16 +1,23 @@
 #include "ffmpeg-wrapper/output-format/IOContextOutputFormat.h"
-#include<ffmpeg-wrapper/ErrorCode.h>
-#include<memory>
+#include <ffmpeg-wrapper/ErrorCode.h>
+#include <memory>
 
 using namespace video;
 
-IOContextOutputFormat::IOContextOutputFormat(std::string url, std::shared_ptr<AVIOContextWrapper> io_context)
+IOContextOutputFormat::IOContextOutputFormat(
+	std::string url,
+	std::shared_ptr<AVIOContextWrapper> io_context)
 {
 	_io_context = io_context;
-	int ret = avformat_alloc_output_context2(&_wrapped_obj, nullptr, nullptr, url.c_str());
+	int ret = avformat_alloc_output_context2(
+		&_wrapped_obj,
+		nullptr,
+		nullptr,
+		url.c_str());
+
 	if (ret < 0)
 	{
-		throw std::runtime_error { ToString((ErrorCode)ret) };
+		throw std::runtime_error{ToString((ErrorCode)ret)};
 	}
 
 	_wrapped_obj->flags |= AVFMT_FLAG_CUSTOM_IO;
--- include/ffmpeg-wrapper/output-format/IOContextOutputFormat.h
+++ include/ffmpeg-wrapper/output-format/IOContextOutputFormat.h
@@ -1,23 +1,21 @@
 #pragma once
-#include<ffmpeg-wrapper/output-format/OutputFormat.h>
-#include<ffmpeg-wrapper/wrapper/AVIOContextWrapper.h>
-#include<memory>
+#include <ffmpeg-wrapper/output-format/OutputFormat.h>
+#include <ffmpeg-wrapper/wrapper/AVIOContextWrapper.h>
+#include <memory>
 
 namespace video
 {
-	class IOContextOutputFormat :public OutputFormat
+	class IOContextOutputFormat : public OutputFormat
 	{
+	private:
 		std::shared_ptr<AVIOContextWrapper> _io_context;
 		AVFormatContext *_wrapped_obj = nullptr;
 
 	public:
-		/// <summary>
-		///		
-		/// </summary>
-		/// <param name="url">这里的 url 不是用来创建文件的，而是让 ffmpeg 根据后缀名分析封装格式的</param>
-		/// <param name="io_context"></param>
+		/// @brief
+		/// @param url 这里的 url 不是用来创建文件的，而是让 ffmpeg 根据后缀名分析封装格式的
+		/// @param io_context
 		IOContextOutputFormat(std::string url, std::shared_ptr<AVIOContextWrapper> io_context);
-
 		~IOContextOutputFormat();
 
 		AVFormatContext *&WrappedObj() override
