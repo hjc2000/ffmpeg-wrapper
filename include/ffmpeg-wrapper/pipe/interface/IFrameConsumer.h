cd264ad85eb7e1cdec61b61de0f79c9e3fe0598e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-30 21:49:23 +0800

    using IFrameConsumer = base::IConsumer<AVFrameWrapper *>;

--- include/ffmpeg-wrapper/pipe/interface/IFrameConsumer.h
+++ include/ffmpeg-wrapper/pipe/interface/IFrameConsumer.h
@@ -4,18 +4,5 @@
 
 namespace video
 {
-	class IFrameConsumer
-	{
-	public:
-		virtual ~IFrameConsumer() = default;
-
-		/// <summary>
-		///		送入帧
-		/// </summary>
-		/// <param name="frame">
-		///		要送入的帧。送入空指针表示冲洗。
-		///		冲洗完后就不要再送入帧了，否则会引发异常。
-		/// </param>
-		virtual void SendData(AVFrameWrapper *frame) = 0;
-	};
+	using IFrameConsumer = base::IConsumer<AVFrameWrapper *>;
 }
