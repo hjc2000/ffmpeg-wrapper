8adfabf5c85d6f5abf1b3e9d5a9c1f3d4f482ad6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-30 22:43:48 +0800

    base::ISource<AVPacketWrapper>

--- include/ffmpeg-wrapper/pipe/interface/IPacketSource.h
+++ include/ffmpeg-wrapper/pipe/interface/IPacketSource.h
@@ -1,17 +1,8 @@
 #pragma once
+#include <base/pipe/ISource.h>
 #include <ffmpeg-wrapper/wrapper/AVPacketWrapper.h>
 
 namespace video
 {
-	class IPacketSource
-	{
-	public:
-		virtual ~IPacketSource() = default;
-
-	public:
-		/// @brief 读取包
-		/// @param packet 用来接收数据的包。
-		/// @return 成功返回 0，失败返回错误代码。
-		virtual int ReadData(AVPacketWrapper &packet) = 0;
-	};
+	using IPacketSource = base::ISource<AVPacketWrapper>;
 }
