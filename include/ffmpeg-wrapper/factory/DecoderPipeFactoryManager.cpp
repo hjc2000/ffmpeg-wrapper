74d27ab4c90c1fcf873b24e264598e4c1d9ff815
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-07 23:38:31 +0800

    std::shared_ptr<video::IDecoderPipeFactory> video::DecoderPipeFactoryManager::DefaultFactory()

--- include/ffmpeg-wrapper/factory/DecoderPipeFactoryManager.cpp
+++ include/ffmpeg-wrapper/factory/DecoderPipeFactoryManager.cpp
@@ -1,18 +1,20 @@
 #include "DecoderPipeFactoryManager.h"
 #include <ffmpeg-wrapper/pipe/DecoderPipe.h>
 
+using namespace video;
+
+class DecoderPipeFactory
+	: public video::IDecoderPipeFactory
+{
+public:
+	std::shared_ptr<video::IDecoderPipe> CreateDecoderPipe(video::AVStreamInfoCollection const &infos) override
+	{
+		return std::shared_ptr<video::IDecoderPipe>{new video::DecoderPipe{infos}};
+	}
+};
+
 std::shared_ptr<video::IDecoderPipeFactory> video::DecoderPipeFactoryManager::DefaultFactory()
 {
-	class DecoderPipeFactory
-		: public video::IDecoderPipeFactory
-	{
-	public:
-		std::shared_ptr<video::IDecoderPipe> CreateDecoderPipe(video::AVStreamInfoCollection const &infos) override
-		{
-			return std::shared_ptr<video::IDecoderPipe>{new video::DecoderPipe{infos}};
-		}
-	};
-
 	static std::shared_ptr<video::IDecoderPipeFactory> o{new DecoderPipeFactory{}};
 	return o;
 }
