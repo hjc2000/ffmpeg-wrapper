9642f749be58937ae732e246a57e9650e490f1c1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-05 01:12:15 +0800

    AudioFrameInfoCollection(AudioFrameInfoCollection const &infos)

--- include/ffmpeg-wrapper/info-collection/AudioFrameInfoCollection.h
+++ include/ffmpeg-wrapper/info-collection/AudioFrameInfoCollection.h
@@ -3,84 +3,103 @@
 
 namespace video
 {
-	/// @brief 音频帧信息集合。
-	class AudioFrameInfoCollection
-		: public IAudioFrameInfoCollection
-	{
-	public:
-		AudioFrameInfoCollection() = default;
+    /// @brief 音频帧信息集合。
+    class AudioFrameInfoCollection :
+        public IAudioFrameInfoCollection
+    {
+    public:
+        AudioFrameInfoCollection() = default;
 
-		/// @brief 构造音频帧信息集合。
-		/// @param infos 音频流信息集合。
-		/// @param nb_samples 音频帧比音频流多了一个信息就是采样点个数。
-		AudioFrameInfoCollection(IAudioStreamInfoCollection const &infos, int nb_samples)
-		{
-			IAudioStreamInfoCollection::operator=(infos);
-			_nb_samples = nb_samples;
-		}
+        /// @brief 构造音频帧信息集合。
+        /// @param infos 音频流信息集合。
+        /// @param nb_samples 音频帧比音频流多了一个信息就是采样点个数。
+        AudioFrameInfoCollection(IAudioStreamInfoCollection const &infos, int nb_samples)
+        {
+            IAudioStreamInfoCollection::operator=(infos);
+            _nb_samples = nb_samples;
+        }
 
-		AudioFrameInfoCollection(IAudioFrameInfoCollection const &infos)
-		{
-			IAudioFrameInfoCollection::operator=(infos);
-		}
+        /// @brief 拷贝构造函数
+        /// @param infos
+        AudioFrameInfoCollection(AudioFrameInfoCollection const &infos)
+        {
+            IAudioFrameInfoCollection::operator=(infos);
+        }
 
-		AudioFrameInfoCollection &operator=(IAudioFrameInfoCollection const &value)
-		{
-			IAudioFrameInfoCollection::operator=(value);
-			return *this;
-		}
+        /// @brief 将 IAudioFrameInfoCollection 的信息拷贝到本对象从而进行构造。
+        /// @param infos
+        AudioFrameInfoCollection(IAudioFrameInfoCollection const &infos)
+        {
+            IAudioFrameInfoCollection::operator=(infos);
+        }
 
-		AVRational _time_base{};
-		AVSampleFormat _sample_format{};
-		int _sample_rate = 0;
-		AVChannelLayout _ch_layout{};
-		int _nb_samples = 0;
+        /// @brief 赋值运算符。
+        /// @param value
+        /// @return
+        AudioFrameInfoCollection &operator=(AudioFrameInfoCollection const &value)
+        {
+            IAudioFrameInfoCollection::operator=(value);
+            return *this;
+        }
+
+        AVRational _time_base{};
+        AVSampleFormat _sample_format{};
+        int _sample_rate = 0;
+        AVChannelLayout _ch_layout{};
+        int _nb_samples = 0;
 
 #pragma region IAudioFrameInfoCollection
-		AVRational TimeBase() const override
-		{
-			return _time_base;
-		}
-		void SetTimeBase(AVRational value) override
-		{
-			_time_base = value;
-		}
 
-		AVSampleFormat SampleFormat() const override
-		{
-			return _sample_format;
-		}
-		void SetSampleFormat(AVSampleFormat value) override
-		{
-			_sample_format = value;
-		}
+        AVRational TimeBase() const override
+        {
+            return _time_base;
+        }
 
-		int SampleRate() const override
-		{
-			return _sample_rate;
-		}
-		void SetSampleRate(int value) override
-		{
-			_sample_rate = value;
-		}
+        void SetTimeBase(AVRational value) override
+        {
+            _time_base = value;
+        }
 
-		AVChannelLayout ChannelLayout() const override
-		{
-			return _ch_layout;
-		}
-		void SetChannelLayout(AVChannelLayout value) override
-		{
-			_ch_layout = value;
-		}
+        AVSampleFormat SampleFormat() const override
+        {
+            return _sample_format;
+        }
 
-		int SampleCount() const override
-		{
-			return _nb_samples;
-		}
-		void SetSampleCount(int value) override
-		{
-			_nb_samples = value;
-		}
+        void SetSampleFormat(AVSampleFormat value) override
+        {
+            _sample_format = value;
+        }
+
+        int SampleRate() const override
+        {
+            return _sample_rate;
+        }
+
+        void SetSampleRate(int value) override
+        {
+            _sample_rate = value;
+        }
+
+        AVChannelLayout ChannelLayout() const override
+        {
+            return _ch_layout;
+        }
+
+        void SetChannelLayout(AVChannelLayout value) override
+        {
+            _ch_layout = value;
+        }
+
+        int SampleCount() const override
+        {
+            return _nb_samples;
+        }
+
+        void SetSampleCount(int value) override
+        {
+            _nb_samples = value;
+        }
+
 #pragma endregion
-	};
-}
+    };
+} // namespace video
