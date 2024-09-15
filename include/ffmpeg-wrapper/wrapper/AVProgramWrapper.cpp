2582d6ef2273281b0e133bbe895e203e5a30114f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-15 16:12:57 +0800

    注释

--- include/ffmpeg-wrapper/wrapper/AVProgramWrapper.cpp
+++ include/ffmpeg-wrapper/wrapper/AVProgramWrapper.cpp
@@ -1,1 +1,15 @@
 #include "AVProgramWrapper.h"
+
+video::AVProgramWrapper::AVProgramWrapper(AVProgram *program)
+{
+    _wrapped_obj = program;
+
+    _dic = std::unique_ptr<AVDictionaryWrapper>{
+        new AVDictionaryWrapper{
+            _wrapped_obj->metadata,
+        },
+    };
+
+    // 需要设置为不释放字典，否则会与 avformat_free_context 函数重复释放。
+    _dic->DoNotFreeInnerDictionary();
+}
--- include/ffmpeg-wrapper/wrapper/AVProgramWrapper.h
+++ include/ffmpeg-wrapper/wrapper/AVProgramWrapper.h
@@ -32,19 +32,7 @@ namespace video
         /// @brief 包装一个 AVProgram 对象，但不拥有它，不会释放 AVProgram 对象内的资源。
         /// AVProgram 对象的主人是 AVFormatContext，avformat_free_context 函数会释放它的资源。
         /// @param program
-        AVProgramWrapper(AVProgram *program)
-        {
-            _wrapped_obj = program;
-
-            _dic = std::unique_ptr<AVDictionaryWrapper>{
-                new AVDictionaryWrapper{
-                    _wrapped_obj->metadata,
-                },
-            };
-
-            // 需要设置为不释放字典，否则会与 avformat_free_context 函数重复释放。
-            _dic->DoNotFreeInnerDictionary();
-        }
+        AVProgramWrapper(AVProgram *program);
 
         AVProgramWrapper(AVProgramWrapper const &o)
         {
