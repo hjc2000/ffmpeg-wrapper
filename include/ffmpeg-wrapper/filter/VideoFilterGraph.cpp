58a119236805e5e371eab365a0b33baa170807d1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-28 21:56:15 +0800

    格式

--- include/ffmpeg-wrapper/filter/VideoFilterGraph.cpp
+++ include/ffmpeg-wrapper/filter/VideoFilterGraph.cpp
@@ -1,154 +1,152 @@
 #include "ffmpeg-wrapper/filter/VideoFilterGraph.h"
 #include "VideoFilterGraph.h"
 
-using namespace video;
-
-VideoFilterGraph::VideoFilterGraph(IVideoStreamInfoCollection const &infos)
+video::VideoFilterGraph::VideoFilterGraph(IVideoStreamInfoCollection const &infos)
 {
-    _input_video_stream_infos = infos;
-    _wrapped_obj = avfilter_graph_alloc();
-    if (_wrapped_obj == nullptr)
-    {
-        throw std::runtime_error{"avfilter_graph_alloc 失败。"};
-    }
+	_input_video_stream_infos = infos;
+	_wrapped_obj = avfilter_graph_alloc();
+	if (_wrapped_obj == nullptr)
+	{
+		throw std::runtime_error{"avfilter_graph_alloc 失败。"};
+	}
 
-    init_buffer_filter();
-    init_buffer_sink_filter();
+	init_buffer_filter();
+	init_buffer_sink_filter();
 }
 
-void VideoFilterGraph::init_buffer_filter()
+void video::VideoFilterGraph::init_buffer_filter()
 {
-    AVFilter const *buffersrc = avfilter_get_by_name("buffer");
+	AVFilter const *buffersrc = avfilter_get_by_name("buffer");
 
-    AVFilterContext *buffersrc_ctx = avfilter_graph_alloc_filter(_wrapped_obj,
-                                                                 buffersrc,
-                                                                 "src");
+	AVFilterContext *buffersrc_ctx = avfilter_graph_alloc_filter(_wrapped_obj,
+																 buffersrc,
+																 "src");
 
-    if (!buffersrc_ctx)
-    {
-        throw std::runtime_error{"avfilter_graph_alloc_filter 失败。"};
-    }
+	if (!buffersrc_ctx)
+	{
+		throw std::runtime_error{"avfilter_graph_alloc_filter 失败。"};
+	}
 
-    av_opt_set_int(buffersrc_ctx,
-                   "width",
-                   _input_video_stream_infos.Width(),
-                   AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_int(buffersrc_ctx,
+				   "width",
+				   _input_video_stream_infos.Width(),
+				   AV_OPT_SEARCH_CHILDREN);
 
-    av_opt_set_int(buffersrc_ctx,
-                   "height",
-                   _input_video_stream_infos.Height(),
-                   AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_int(buffersrc_ctx,
+				   "height",
+				   _input_video_stream_infos.Height(),
+				   AV_OPT_SEARCH_CHILDREN);
 
-    av_opt_set_pixel_fmt(buffersrc_ctx,
-                         "pix_fmt",
-                         _input_video_stream_infos.PixelFormat(),
-                         AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_pixel_fmt(buffersrc_ctx,
+						 "pix_fmt",
+						 _input_video_stream_infos.PixelFormat(),
+						 AV_OPT_SEARCH_CHILDREN);
 
-    av_opt_set_q(buffersrc_ctx,
-                 "time_base",
-                 _input_video_stream_infos.TimeBase(),
-                 AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_q(buffersrc_ctx,
+				 "time_base",
+				 _input_video_stream_infos.TimeBase(),
+				 AV_OPT_SEARCH_CHILDREN);
 
-    av_opt_set_q(buffersrc_ctx,
-                 "FrameRate",
-                 _input_video_stream_infos.FrameRate(),
-                 AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_q(buffersrc_ctx,
+				 "FrameRate",
+				 _input_video_stream_infos.FrameRate(),
+				 AV_OPT_SEARCH_CHILDREN);
 
-    int ret = avfilter_init_str(buffersrc_ctx, nullptr);
-    if (ret < 0)
-    {
-        throw std::runtime_error{base::ToString((ErrorCode)ret)};
-    }
+	int ret = avfilter_init_str(buffersrc_ctx, nullptr);
+	if (ret < 0)
+	{
+		throw std::runtime_error{base::ToString((ErrorCode)ret)};
+	}
 
-    _buffer_filter = AVFilterContextWrapper{buffersrc_ctx};
+	_buffer_filter = AVFilterContextWrapper{buffersrc_ctx};
 }
 
-void VideoFilterGraph::init_buffer_sink_filter()
+void video::VideoFilterGraph::init_buffer_sink_filter()
 {
-    AVFilter const *buffersink = avfilter_get_by_name("buffersink");
+	AVFilter const *buffersink = avfilter_get_by_name("buffersink");
 
-    AVFilterContext *buffersink_ctx = avfilter_graph_alloc_filter(_wrapped_obj,
-                                                                  buffersink,
-                                                                  "sink");
+	AVFilterContext *buffersink_ctx = avfilter_graph_alloc_filter(_wrapped_obj,
+																  buffersink,
+																  "sink");
 
-    if (buffersink_ctx == nullptr)
-    {
-        throw std::runtime_error{"AVFilterContext 构造失败"};
-    }
+	if (buffersink_ctx == nullptr)
+	{
+		throw std::runtime_error{"AVFilterContext 构造失败"};
+	}
 
-    if (avfilter_init_str(buffersink_ctx, nullptr) < 0)
-    {
-        throw std::runtime_error{"AVFilterContext 初始化失败"};
-    }
+	if (avfilter_init_str(buffersink_ctx, nullptr) < 0)
+	{
+		throw std::runtime_error{"AVFilterContext 初始化失败"};
+	}
 
-    _buffer_sink_filter = AVFilterContextWrapper{buffersink_ctx};
+	_buffer_sink_filter = AVFilterContextWrapper{buffersink_ctx};
 }
 
-AVFilterContextWrapper &VideoFilterGraph::buffer_filter()
+video::AVFilterContextWrapper &video::VideoFilterGraph::buffer_filter()
 {
-    return _buffer_filter;
+	return _buffer_filter;
 }
 
-AVFilterContextWrapper &VideoFilterGraph::buffer_sink_filter()
+video::AVFilterContextWrapper &video::VideoFilterGraph::buffer_sink_filter()
 {
-    return _buffer_sink_filter;
+	return _buffer_sink_filter;
 }
 
-AVFilterContextWrapper VideoFilterGraph::alloc_fps_filter(AVRational FrameRate)
+video::AVFilterContextWrapper video::VideoFilterGraph::alloc_fps_filter(AVRational FrameRate)
 {
-    AVFilter const *fps_filter = avfilter_get_by_name("fps");
-    AVFilterContext *fps_ctx = avfilter_graph_alloc_filter(_wrapped_obj, fps_filter, "fps");
-    if (fps_ctx == nullptr)
-    {
-        throw std::runtime_error{"AVFilterContext 构造失败"};
-    }
+	AVFilter const *fps_filter = avfilter_get_by_name("fps");
+	AVFilterContext *fps_ctx = avfilter_graph_alloc_filter(_wrapped_obj, fps_filter, "fps");
+	if (fps_ctx == nullptr)
+	{
+		throw std::runtime_error{"AVFilterContext 构造失败"};
+	}
 
-    AVDictionaryWrapper options;
-    options.SetValueByKey("fps", base::ToString(FrameRate).c_str());
-    if (avfilter_init_dict(fps_ctx, &options.WrappedObj()) < 0)
-    {
-        throw std::runtime_error{"AVFilterContext 初始化失败"};
-    }
+	AVDictionaryWrapper options;
+	options.SetValueByKey("fps", base::ToString(FrameRate).c_str());
+	if (avfilter_init_dict(fps_ctx, &options.WrappedObj()) < 0)
+	{
+		throw std::runtime_error{"AVFilterContext 初始化失败"};
+	}
 
-    return AVFilterContextWrapper{fps_ctx};
+	return AVFilterContextWrapper{fps_ctx};
 }
 
-void VideoFilterGraph::config_graph()
+void video::VideoFilterGraph::config_graph()
 {
-    int ret = avfilter_graph_config(_wrapped_obj, nullptr);
-    if (ret < 0)
-    {
-        throw std::runtime_error{base::ToString((ErrorCode)ret)};
-    }
+	int ret = avfilter_graph_config(_wrapped_obj, nullptr);
+	if (ret < 0)
+	{
+		throw std::runtime_error{base::ToString((ErrorCode)ret)};
+	}
 }
 
-int VideoFilterGraph::ReadData(AVFrameWrapper &frame)
+int video::VideoFilterGraph::ReadData(AVFrameWrapper &frame)
 {
-    return av_buffersink_get_frame(_buffer_sink_filter, frame);
+	return av_buffersink_get_frame(_buffer_sink_filter, frame);
 }
 
-void VideoFilterGraph::SendData(AVFrameWrapper &frame)
+void video::VideoFilterGraph::SendData(AVFrameWrapper &frame)
 {
-    // 非冲洗模式
-    int ret = av_buffersrc_add_frame_flags(_buffer_filter,
-                                           frame,
-                                           AV_BUFFERSRC_FLAG_KEEP_REF);
+	// 非冲洗模式
+	int ret = av_buffersrc_add_frame_flags(_buffer_filter,
+										   frame,
+										   AV_BUFFERSRC_FLAG_KEEP_REF);
 
-    if (ret < 0)
-    {
-        throw std::runtime_error{base::ToString((ErrorCode)ret)};
-    }
+	if (ret < 0)
+	{
+		throw std::runtime_error{base::ToString((ErrorCode)ret)};
+	}
 }
 
 void video::VideoFilterGraph::Flush()
 {
-    // 冲洗模式
-    int ret = av_buffersrc_add_frame_flags(_buffer_filter,
-                                           nullptr,
-                                           AV_BUFFERSRC_FLAG_KEEP_REF);
+	// 冲洗模式
+	int ret = av_buffersrc_add_frame_flags(_buffer_filter,
+										   nullptr,
+										   AV_BUFFERSRC_FLAG_KEEP_REF);
 
-    if (ret < 0)
-    {
-        throw std::runtime_error{base::ToString((ErrorCode)ret)};
-    }
+	if (ret < 0)
+	{
+		throw std::runtime_error{base::ToString((ErrorCode)ret)};
+	}
 }
--- include/ffmpeg-wrapper/filter/VideoFilterGraph.h
+++ include/ffmpeg-wrapper/filter/VideoFilterGraph.h
@@ -8,46 +8,59 @@
 
 namespace video
 {
-    /// @brief 视频滤镜图
-    class VideoFilterGraph :
-        public base::Wrapper<AVFilterGraph>,
-        public base::IConsumer<AVFrameWrapper>,
-        public base::ISource<AVFrameWrapper>
-    {
-    private:
-        AVFilterGraph *_wrapped_obj = nullptr;
-        VideoStreamInfoCollection _input_video_stream_infos;
-        AVFilterContextWrapper _buffer_filter;
-        AVFilterContextWrapper _buffer_sink_filter;
-        void init_buffer_filter();
-        void init_buffer_sink_filter();
+	/**
+	 * @brief 视频滤镜图
+	 *
+	 */
+	class VideoFilterGraph :
+		public base::Wrapper<AVFilterGraph>,
+		public base::IConsumer<AVFrameWrapper>,
+		public base::ISource<AVFrameWrapper>
+	{
+	private:
+		AVFilterGraph *_wrapped_obj = nullptr;
+		VideoStreamInfoCollection _input_video_stream_infos;
+		AVFilterContextWrapper _buffer_filter;
+		AVFilterContextWrapper _buffer_sink_filter;
+		void init_buffer_filter();
+		void init_buffer_sink_filter();
 
-    public:
-        VideoFilterGraph(IVideoStreamInfoCollection const &infos);
+	public:
+		VideoFilterGraph(IVideoStreamInfoCollection const &infos);
 
-        AVFilterGraph *WrappedObj() const override
-        {
-            return _wrapped_obj;
-        }
+	public:
+		AVFilterGraph *WrappedObj() const override
+		{
+			return _wrapped_obj;
+		}
 
-        /// @brief 获取本滤镜图的起点
-        /// @return
-        AVFilterContextWrapper &buffer_filter();
+		/**
+		 * @brief 获取本滤镜图的起点
+		 *
+		 * @return AVFilterContextWrapper&
+		 */
+		AVFilterContextWrapper &buffer_filter();
 
-        /// @brief 获取本滤镜图的终点
-        /// @return
-        AVFilterContextWrapper &buffer_sink_filter();
+		/**
+		 * @brief 获取本滤镜图的终点
+		 *
+		 * @return AVFilterContextWrapper&
+		 */
+		AVFilterContextWrapper &buffer_sink_filter();
 
-        /// @brief 构造一个 fps 滤镜
-        /// @param FrameRate
-        /// @return
-        AVFilterContextWrapper alloc_fps_filter(AVRational FrameRate);
+		/**
+		 * @brief 构造一个 fps 滤镜
+		 *
+		 * @param FrameRate
+		 * @return AVFilterContextWrapper
+		 */
+		AVFilterContextWrapper alloc_fps_filter(AVRational FrameRate);
 
-        void config_graph();
+		void config_graph();
 
-        int ReadData(AVFrameWrapper &frame) override;
-        void SendData(AVFrameWrapper &frame) override;
+		int ReadData(AVFrameWrapper &frame) override;
+		void SendData(AVFrameWrapper &frame) override;
 
-        void Flush() override;
-    };
+		void Flush() override;
+	};
 } // namespace video
