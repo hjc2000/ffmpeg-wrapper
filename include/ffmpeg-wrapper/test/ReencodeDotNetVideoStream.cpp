95ad6fcd16001b241f1f1eeb7cda9f0764081416
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-28 22:05:19 +0800

    video::

--- include/ffmpeg-wrapper/test/ReencodeDotNetVideoStream.cpp
+++ include/ffmpeg-wrapper/test/ReencodeDotNetVideoStream.cpp
@@ -5,88 +5,85 @@
 #include <ffmpeg-wrapper/factory/EncoderPipeFactoryManager.h>
 #include <ffmpeg-wrapper/info-collection/VideoStreamInfoCollection.h>
 
-using namespace video;
-
 void ReencodeDotNetVideoStream(DotNetStream *dotnet_video_stream)
 {
-    // 想要输出的视频信息
-    VideoStreamInfoCollection output_video_stream_infos;
-    output_video_stream_infos._frame_rate = AVRational{30, 1};
-    output_video_stream_infos._width = 1920;
-    output_video_stream_infos._height = 1080;
-    output_video_stream_infos._pixel_format = AVPixelFormat::AV_PIX_FMT_YUV420P;
+	// 想要输出的视频信息
+	video::VideoStreamInfoCollection output_video_stream_infos;
+	output_video_stream_infos._frame_rate = AVRational{30, 1};
+	output_video_stream_infos._width = 1920;
+	output_video_stream_infos._height = 1080;
+	output_video_stream_infos._pixel_format = AVPixelFormat::AV_PIX_FMT_YUV420P;
 
-    // 想要输出的音频信息
-    AudioStreamInfoCollection output_audio_stream_infos;
-    output_audio_stream_infos._ch_layout = AVChannelLayoutExtension::GetDefaultChannelLayout(2);
-    output_audio_stream_infos._sample_format = AVSampleFormat::AV_SAMPLE_FMT_FLTP;
-    output_audio_stream_infos._sample_rate = 48000;
+	// 想要输出的音频信息
+	video::AudioStreamInfoCollection output_audio_stream_infos;
+	output_audio_stream_infos._ch_layout = video::AVChannelLayoutExtension::GetDefaultChannelLayout(2);
+	output_audio_stream_infos._sample_format = AVSampleFormat::AV_SAMPLE_FMT_FLTP;
+	output_audio_stream_infos._sample_rate = 48000;
 
-    // 编码封装管道
-    std::shared_ptr<base::Stream> out_fs = base::di::filesystem::CreateNewAnyway("mux_out.ts");
-    std::shared_ptr<StreamOutputFormat> out_fmt_ctx{new StreamOutputFormat{".ts", out_fs}};
+	// 编码封装管道
+	std::shared_ptr<base::Stream> out_fs = base::di::filesystem::CreateNewAnyway("mux_out.ts");
 
-    std::shared_ptr<SptsEncodeMux> spts_encode_mux{
-        new SptsEncodeMux{
-            out_fmt_ctx,
-            output_video_stream_infos,
-            "hevc_amf",
-            -1,
-            output_audio_stream_infos,
-            "aac",
-        },
-    };
+	std::shared_ptr<video::StreamOutputFormat> out_fmt_ctx{new video::StreamOutputFormat{
+		".ts",
+		out_fs,
+	}};
 
-    // 输入格式
-    std::shared_ptr<JoinedInputFormatDemuxDecoder> joined_input_format_demux_decoder{
-        new JoinedInputFormatDemuxDecoder{}};
-    joined_input_format_demux_decoder->AddVideoFrameConsumer(
-        spts_encode_mux->VideoEncodePipe());
-    joined_input_format_demux_decoder->AddAudioFrameConsumer(
-        spts_encode_mux->AudioEncodePipe());
+	std::shared_ptr<video::SptsEncodeMux> spts_encode_mux{new video::SptsEncodeMux{
+		out_fmt_ctx,
+		output_video_stream_infos,
+		"hevc_amf",
+		-1,
+		output_audio_stream_infos,
+		"aac",
+	}};
 
-    int loop_times = 0;
+	// 输入格式
+	std::shared_ptr<video::JoinedInputFormatDemuxDecoder> joined_input_format_demux_decoder{new video::JoinedInputFormatDemuxDecoder{}};
+	joined_input_format_demux_decoder->AddVideoFrameConsumer(spts_encode_mux->VideoEncodePipe());
+	joined_input_format_demux_decoder->AddAudioFrameConsumer(spts_encode_mux->AudioEncodePipe());
 
-    auto get_input_format_func = [&](std::shared_ptr<InputFormat> &current_input_format)
-    {
-        if (loop_times > 2)
-        {
-            return;
-        }
+	int loop_times = 0;
 
-        dotnet_video_stream->SetPosition(0);
-        std::shared_ptr<InputFormat> in_fmt_ctx{new InputFormat{dotnet_video_stream->ToSharePtr()}};
-        loop_times++;
-        current_input_format = in_fmt_ctx;
-    };
-    joined_input_format_demux_decoder->NeedInputFormatEvent().Subscribe(get_input_format_func);
+	auto get_input_format_func = [&](std::shared_ptr<video::InputFormat> &current_input_format)
+	{
+		if (loop_times > 2)
+		{
+			return;
+		}
 
-    // 解码管道
-    base::CancellationTokenSource cancel_pump_source;
-    base::TaskCompletionSignal pump_thread_exit{false};
+		dotnet_video_stream->SetPosition(0);
+		std::shared_ptr<video::InputFormat> in_fmt_ctx{new video::InputFormat{dotnet_video_stream->ToSharePtr()}};
+		loop_times++;
+		current_input_format = in_fmt_ctx;
+	};
+	joined_input_format_demux_decoder->NeedInputFormatEvent().Subscribe(get_input_format_func);
 
-    auto thread_fun = [&]()
-    {
-        try
-        {
-            joined_input_format_demux_decoder->PumpDataToConsumers(cancel_pump_source.Token());
-        }
-        catch (std::exception &e)
-        {
-            std::cerr << e.what() << std::endl;
-        }
-        catch (...)
-        {
-            std::cerr << "发生未知异常" << std::endl;
-        }
+	// 解码管道
+	base::CancellationTokenSource cancel_pump_source;
+	base::TaskCompletionSignal pump_thread_exit{false};
 
-        std::cout << "线程退出" << std::endl;
-        pump_thread_exit.SetResult();
-    };
+	auto thread_fun = [&]()
+	{
+		try
+		{
+			joined_input_format_demux_decoder->PumpDataToConsumers(cancel_pump_source.Token());
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "发生未知异常" << std::endl;
+		}
 
-    std::thread(thread_fun).detach();
+		std::cout << "线程退出" << std::endl;
+		pump_thread_exit.SetResult();
+	};
 
-    std::cin.get();
-    cancel_pump_source.Cancel();
-    pump_thread_exit.Wait();
+	std::thread(thread_fun).detach();
+
+	std::cin.get();
+	cancel_pump_source.Cancel();
+	pump_thread_exit.Wait();
 }
--- include/ffmpeg-wrapper/test/ReencodeDotNetVideoStream.h
+++ include/ffmpeg-wrapper/test/ReencodeDotNetVideoStream.h
@@ -6,14 +6,13 @@
 #include <ffmpeg-wrapper/input-format/InputFormat.h>
 #include <ffmpeg-wrapper/mux/SptsEncodeMux.h>
 #include <ffmpeg-wrapper/output-format/StreamOutputFormat.h>
-#include <iostream>
 #include <pinvoke/DotNetStream.h>
 
 extern "C"
 {
-    /// <summary>
-    ///		C# 打开一个文件流，传过来进行重新编码。
-    /// </summary>
-    /// <param name="dotnet_video_stream"></param>
-    void ReencodeDotNetVideoStream(DotNetStream *dotnet_video_stream);
+	/// <summary>
+	///		C# 打开一个文件流，传过来进行重新编码。
+	/// </summary>
+	/// <param name="dotnet_video_stream"></param>
+	void ReencodeDotNetVideoStream(DotNetStream *dotnet_video_stream);
 }
