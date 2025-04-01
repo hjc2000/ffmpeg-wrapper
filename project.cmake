c4771e53e2169e86bee38b95a16a6c3614e0f9d9
hjc2000 <hjc.666@foxmail.com>
2025-04-01 17:11:39 +0800

    project.cmake

---
+++ project.cmake
@@ -0,0 +1,19 @@
+# 编译为动态库
+add_library(${ProjectName})
+target_import_src(${ProjectName})
+target_import_ffmpeg(${ProjectName} PUBLIC)
+target_import_base(${ProjectName} PUBLIC)
+target_import_pinvoke(${ProjectName} PUBLIC)
+
+# 添加测试程序
+if(1)
+	set(exe_name "test")
+	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
+	target_compile_definitions(${exe_name} PRIVATE Predefine_ResourceDir="D:/video/视频开发测试")
+
+	target_link_libraries(${ProjectName} PUBLIC -Wl,--start-group)
+	target_link_libraries(${exe_name} ${ProjectName})
+	target_link_libraries(${ProjectName} PUBLIC -Wl,--end-group)
+
+	target_install(${exe_name})
+endif()
