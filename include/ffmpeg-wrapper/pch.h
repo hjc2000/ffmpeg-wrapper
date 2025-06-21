43731628284885e97b6269140eaae03ab98b7f75
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-21 15:18:04 +0800

    更新FFmpeg头文件引用格式并添加extern "C"闭合标记

--- include/ffmpeg-wrapper/pch.h
+++ include/ffmpeg-wrapper/pch.h
@@ -4,25 +4,27 @@
 
 extern "C"
 {
-#include <libavcodec/avcodec.h>
-#include <libavfilter/buffersink.h>
-#include <libavfilter/buffersrc.h>
-#include <libavformat/avformat.h>
-#include <libavformat/avio.h>
-#include <libavutil/audio_fifo.h>
-#include <libavutil/avutil.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/error.h>
-#include <libavutil/fifo.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/log.h>
-#include <libavutil/opt.h>
-#include <libavutil/pixdesc.h>
-#include <libavutil/samplefmt.h>
-#include <libavutil/timestamp.h>
-#include <libswresample/swresample.h>
-#include <libswscale/swscale.h>
-}
+
+#include "libavcodec/avcodec.h"
+#include "libavfilter/buffersink.h"
+#include "libavfilter/buffersrc.h"
+#include "libavformat/avformat.h"
+#include "libavformat/avio.h"
+#include "libavutil/audio_fifo.h"
+#include "libavutil/avutil.h"
+#include "libavutil/channel_layout.h"
+#include "libavutil/error.h"
+#include "libavutil/fifo.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/log.h"
+#include "libavutil/opt.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/timestamp.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
+
+} // extern "C"
 
 #include <base/pch.h>
 
